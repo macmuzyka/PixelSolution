9dd2920198a9816ced33ba826839422ba9ce63ba
macmuzyka <raweshau@gmail.com>
2021-06-28 20:47:38 +0200

    update with correct database query

--- src/main/java/com/pixel/model/repository/PatientRepository.java
+++ src/main/java/com/pixel/model/repository/PatientRepository.java
@@ -1,7 +1,7 @@
 package com.pixel.model.repository;
 
 import com.pixel.model.Patient;
-import com.pixel.model.non_entity_projection.ResultDTO;
+import com.pixel.model.non_entity_projection.PatientVisitsDTO;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -14,19 +14,23 @@ import java.util.Optional;
 public interface PatientRepository extends CrudRepository<Patient, Integer> {
 
     @Query(nativeQuery = true, value =
-            "SELECT FIRST_NAME, LAST_NAME, COUNT(*) AS VISIT_COUNTER FROM PATIENTS " +
-                    "INNER JOIN VISITS ON PATIENTS.ID = VISITS.PATIENT_ID " +
-                    "INNER JOIN PRACTITIONERS ON PRACTITIONERS.ID = VISITS.PRACTITIONER_ID " +
+            "SELECT FIRST_NAME, LAST_NAME, COUNT(*) AS visitCounter " +
+                    "FROM PATIENTS p " +
+                    "         INNER JOIN VISITS v ON p.ID = v.PATIENT_ID " +
+                    "         INNER JOIN PRACTITIONERS pr ON v.PRACTITIONER_ID = pr.ID " +
                     "WHERE CASE " +
-                    "           WHEN SPECIALIZATION != 'ALL' OR SPECIALIZATION IS NOT NULL THEN" +
-                    "                 SPECIALIZATION = :specialization " +
-                    "       END " +
-                    "AND" +
-                    "      CASE" +
-                    "           WHEN CITY != 'ALL' OR CITY IS NOT NULL THEN" +
-                    "                CITY = :city" +
-                    "            END " +
+                    "          WHEN :specialization = 'ALL' OR :specialization IS NULL THEN " +
+                    "                  pr.SPECIALIZATION LIKE '%' " +
+                    "          ELSE " +
+                    "                  pr.SPECIALIZATION = :specialization " +
+                    "    END " +
+                    "  AND CASE " +
+                    "          WHEN :city = 'ALL' OR p.CITY IS NULL THEN " +
+                    "                  p.CITY LIKE '%' " +
+                    "          ELSE " +
+                    "                  p.CITY = :city " +
+                    "    END " +
                     "GROUP BY FIRST_NAME")
-    Optional<List<ResultDTO>> countPatientVisits(@Param("city") String city,
-                                                 @Param("specialization") String specialization);
+    Optional<List<PatientVisitsDTO>> countPatientVisits(@Param("city") String city,
+                                                        @Param("specialization") String specialization);
 }
