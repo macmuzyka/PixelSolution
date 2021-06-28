af0197a1158ef94f6dbf3f55482431bf6cad4dca
macmuzyka <raweshau@gmail.com>
2021-06-28 20:49:31 +0200

    update practitioner repository with correct query

--- src/main/java/com/pixel/model/repository/PractitionerRepository.java
+++ src/main/java/com/pixel/model/repository/PractitionerRepository.java
@@ -1,9 +1,26 @@
 package com.pixel.model.repository;
 
 import com.pixel.model.Practitioner;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.pixel.model.non_entity_projection.PractitionerVisitsDTO;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface PractitionerRepository extends JpaRepository<Practitioner, Integer> {
+public interface PractitionerRepository extends CrudRepository<Practitioner, Integer> {
+    @Query(nativeQuery = true, value = "SELECT pr.SPECIALIZATION, COUNT(*) AS TOTAL_VISITS " +
+            "FROM PRACTITIONERS pr " +
+            "         INNER JOIN VISITS v ON pr.ID = v.PRACTITIONER_ID " +
+            "WHERE CASE " +
+            "          WHEN :specialization = 'ALL' OR pr.SPECIALIZATION IS NULL " +
+            "              THEN pr.SPECIALIZATION LIKE '%' " +
+            "          ELSE " +
+            "              pr.SPECIALIZATION = :specialization " +
+            "          END " +
+            "GROUP BY pr.SPECIALIZATION")
+    Optional<List<PractitionerVisitsDTO>> findPractitionerVisits(@Param("specialization") String specialization);
 }
