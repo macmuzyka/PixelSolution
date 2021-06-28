3ae91c7c0f6a30e54324403b2b71015f93bce296
macmuzyka <raweshau@gmail.com>
2021-06-28 23:00:59 +0200

    update query with variable for input is null condition

--- src/main/java/com/pixel/model/repository/PractitionerRepository.java
+++ src/main/java/com/pixel/model/repository/PractitionerRepository.java
@@ -2,8 +2,8 @@ package com.pixel.model.repository;
 
 import com.pixel.model.Practitioner;
 import com.pixel.model.non_entity_projection.PractitionerVisitsDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,12 +11,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PractitionerRepository extends CrudRepository<Practitioner, Integer> {
+public interface PractitionerRepository extends JpaRepository<Practitioner, Integer> {
     @Query(nativeQuery = true, value = "SELECT pr.SPECIALIZATION, COUNT(*) AS TOTAL_VISITS " +
             "FROM PRACTITIONERS pr " +
             "         INNER JOIN VISITS v ON pr.ID = v.PRACTITIONER_ID " +
             "WHERE CASE " +
-            "          WHEN :specialization = 'ALL' OR pr.SPECIALIZATION IS NULL " +
+            "          WHEN :specialization = 'ALL' OR :specialization IS NULL " +
             "              THEN pr.SPECIALIZATION LIKE '%' " +
             "          ELSE " +
             "              pr.SPECIALIZATION = :specialization " +
