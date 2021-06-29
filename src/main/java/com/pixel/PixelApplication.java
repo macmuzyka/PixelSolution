6e8929105fead2a51a67247becab227d9f1870bd
macmuzyka <raweshau@gmail.com>
2021-06-29 22:28:46 +0200

    add ObjectMapper @Bean

--- src/main/java/com/pixel/PixelApplication.java
+++ src/main/java/com/pixel/PixelApplication.java
@@ -1,12 +1,19 @@
 package com.pixel;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PixelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PixelApplication.class, args);
+    }
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
     }
 }
