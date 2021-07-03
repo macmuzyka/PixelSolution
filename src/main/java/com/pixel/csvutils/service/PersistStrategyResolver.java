7a7c9ff122ce710855fb33a0d44614d64d71012c
macmuzyka <raweshau@gmail.com>
2021-07-03 23:15:19 +0200

    add resolver for strategy implementation of file persistence based on file

---
+++ src/main/java/com/pixel/csvutils/service/PersistStrategyResolver.java
@@ -0,0 +1,61 @@
+package com.pixel.csvutils.service;
+
+import com.pixel.advice.IllegalPersistStrategy;
+import com.pixel.csvutils.handler.PatientFile;
+import com.pixel.csvutils.handler.PatientToPractitionerFile;
+import com.pixel.csvutils.handler.PractitionerFile;
+import com.pixel.csvutils.handler.VisitFile;
+import com.pixel.model.repository.PatientRepository;
+import com.pixel.model.repository.PatientToPractitionerRepository;
+import com.pixel.model.repository.PractitionerRepository;
+import com.pixel.model.repository.VisitRepository;
+
+/**
+ * Created by Maciej Muzyka
+ * on 01.07.2021
+ */
+
+public class PersistStrategyResolver {
+    private final PatientToPractitionerRepository patientToPractitionerRepository;
+    private final PatientToPractitionerFile patientToPractitionerFile;
+    private final PatientRepository patientRepository;
+    private final PatientFile patientFile;
+    private final PractitionerRepository practitionerRepository;
+    private final PractitionerFile practitionerFile;
+    private final VisitRepository visitRepository;
+    private final VisitFile visitFile;
+
+    public PersistStrategyResolver(
+            final PatientFile patientFile,
+            final PatientRepository patientRepository,
+            final VisitFile visitFile,
+            final VisitRepository visitRepository,
+            final PractitionerFile practitionerFile,
+            final PractitionerRepository practitionerRepository,
+            final PatientToPractitionerFile patientToPractitionerFile,
+            final PatientToPractitionerRepository patientToPractitionerRepository) {
+        this.patientToPractitionerRepository = patientToPractitionerRepository;
+        this.patientToPractitionerFile = patientToPractitionerFile;
+        this.patientRepository = patientRepository;
+        this.patientFile = patientFile;
+        this.practitionerRepository = practitionerRepository;
+        this.practitionerFile = practitionerFile;
+        this.visitRepository = visitRepository;
+        this.visitFile = visitFile;
+    }
+
+    public CSVPersistence resolve(String file) {
+        switch (file) {
+            case "patient2practitioner":
+                return new P2PRecords(patientToPractitionerRepository, patientToPractitionerFile);
+            case "patients":
+                return new PatientRecords(patientRepository, patientFile);
+            case "practitioners":
+                return new PractitionerRecords(practitionerRepository, practitionerFile);
+            case "visits":
+                return new VisitRecords(visitRepository, visitFile);
+            default:
+                throw new IllegalPersistStrategy("Persist strategy not resolved!");
+        }
+    }
+}
