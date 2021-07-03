194b78835b6b8090cf1445b71249b029b372d2c6
macmuzyka <raweshau@gmail.com>
2021-07-03 23:10:06 +0200

    add base model class for generic purposes

---
+++ src/main/java/com/pixel/model/BaseModel.java
@@ -0,0 +1,12 @@
+package com.pixel.model;
+
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Created by Maciej Muzyka
+ * on 03.07.2021
+ */
+
+@MappedSuperclass
+public abstract class BaseModel {
+}
