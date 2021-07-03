4717706d3e6287480c8789c4da40997cad219d69
macmuzyka <raweshau@gmail.com>
2021-07-03 23:16:21 +0200

    change access modifier

--- src/main/java/com/pixel/csvutils/service/CSVPersistence.java
+++ src/main/java/com/pixel/csvutils/service/CSVPersistence.java
@@ -4,6 +4,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-interface CSVService {
+public interface CSVPersistence {
     void save(MultipartFile multipartFile) throws IOException;
 }
