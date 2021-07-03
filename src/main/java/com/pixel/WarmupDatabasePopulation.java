8f174907f48e2be4d63fb3eac82d6f4ccddb35b6
macmuzyka <raweshau@gmail.com>
2021-07-03 23:39:57 +0200

    add required classes to satisfy persistence strategy resolver constructor parameters, add method extractions, add strategy resolver attempting to implement strategy design pattern

--- src/main/java/com/pixel/WarmupDatabasePopulation.java
+++ src/main/java/com/pixel/WarmupDatabasePopulation.java
@@ -1,9 +1,15 @@
 package com.pixel;
 
-import com.pixel.csvutils.service.PatientFile;
-import com.pixel.csvutils.service.P2PFile;
-import com.pixel.csvutils.service.PractitionerFile;
-import com.pixel.csvutils.service.VisitsFile;
+import com.pixel.advice.IllegalPersistStrategy;
+import com.pixel.csvutils.handler.PatientFile;
+import com.pixel.csvutils.handler.PatientToPractitionerFile;
+import com.pixel.csvutils.handler.PractitionerFile;
+import com.pixel.csvutils.handler.VisitFile;
+import com.pixel.csvutils.service.*;
+import com.pixel.model.repository.PatientRepository;
+import com.pixel.model.repository.PatientToPractitionerRepository;
+import com.pixel.model.repository.PractitionerRepository;
+import com.pixel.model.repository.VisitRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,54 +33,90 @@ import java.nio.file.Paths;
 class WarmupDatabasePopulation implements ApplicationListener<ApplicationStartedEvent> {
 
     private static final Logger logger = LoggerFactory.getLogger(WarmupDatabasePopulation.class);
-    private final PatientFile patientService;
-    private final PractitionerFile practitionerService;
-    private final VisitsFile visitService;
-    private final P2PFile patientToPractitionerService;
 
-    WarmupDatabasePopulation(final PatientFile patientService,
-                             final PractitionerFile practitionerService,
-                             final VisitsFile visitService,
-                             final P2PFile patientToPractitionerService) {
-        this.patientService = patientService;
-        this.practitionerService = practitionerService;
-        this.visitService = visitService;
-        this.patientToPractitionerService = patientToPractitionerService;
+    private static final String baseResourcesPath = "src/main/resources/";
+    public static final String csvFileExtension = ".csv";
+    private static final String[] filesNames = {"patients", "practitioners", "visits", "patient2practitioner"};
+
+    private final PatientToPractitionerRepository patientToPractitionerRepository;
+    private final PatientRepository patientRepository;
+    private final VisitRepository visitRepository;
+    private final PractitionerRepository practitionerRepository;
+
+    private final PatientFile patientFile;
+    private final VisitFile visitFile;
+    private final PractitionerFile practitionerFile;
+    private final PatientToPractitionerFile patientToPractitionerFile;
+
+    WarmupDatabasePopulation(final PatientToPractitionerRepository patientToPractitionerRepository,
+                             final PatientRepository patientRepository,
+                             final VisitRepository visitRepository,
+                             final PractitionerRepository practitionerRepository,
+                             final PatientFile patientFile,
+                             final VisitFile visitFile,
+                             final PractitionerFile practitionerFile,
+                             final PatientToPractitionerFile patientToPractitionerFile) {
+        this.patientToPractitionerRepository = patientToPractitionerRepository;
+        this.patientRepository = patientRepository;
+        this.visitRepository = visitRepository;
+        this.practitionerRepository = practitionerRepository;
+        this.patientFile = patientFile;
+        this.visitFile = visitFile;
+        this.practitionerFile = practitionerFile;
+        this.patientToPractitionerFile = patientToPractitionerFile;
     }
 
     @Override
     public void onApplicationEvent(final ApplicationStartedEvent applicationStartedEvent) {
-        File patientsCSVFile = new File("src/main/resources/patients.csv");
-        Path patientsCSVFilePath = Paths.get(patientsCSVFile.getPath());
+        int step = 1;
+        final PersistStrategyResolver resolver = new PersistStrategyResolver(
+                patientFile, patientRepository,
+                visitFile, visitRepository,
+                practitionerFile, practitionerRepository,
+                patientToPractitionerFile, patientToPractitionerRepository);
 
-        File practitionersCSVFile = new File("src/main/resources/practitioners.csv");
-        Path practitionersCSVFilePath = Paths.get(practitionersCSVFile.getPath());
+        File currentFile;
+        Path path;
+        byte[] bytes;
 
-        File visitsCSVFile = new File("src/main/resources/visits.csv");
-        Path visitsCSVFilePath = Paths.get(visitsCSVFile.getPath());
-
-        File p2pCSVFile = new File("src/main/resources/patient2practitioner.csv");
-        Path p2pCSVFilePath = Paths.get(p2pCSVFile.getPath());
-
-        byte[] tempByteArray;
-
+        logger.info("Attempting to populate database in warmup application started event...");
         try {
-            logger.info("Application start, attempting to insert data from CSV files to database.");
-            tempByteArray = Files.readAllBytes(patientsCSVFilePath);
-            patientService.save(new MockMultipartFile(patientsCSVFile.getName(), tempByteArray));
+            int filesProgress = 1;
+            for (String file : filesNames) {
+                logger.info(currentFile(file));
 
-            tempByteArray = Files.readAllBytes(practitionersCSVFilePath);
-            practitionerService.save(new MockMultipartFile(practitionersCSVFile.getName(), tempByteArray));
+                currentFile = new File(baseResourcesPath + file + csvFileExtension);
+                path = Paths.get(currentFile.getPath());
 
-            tempByteArray = Files.readAllBytes(visitsCSVFilePath);
-            visitService.save(new MockMultipartFile(visitsCSVFile.getName(), tempByteArray));
+                CSVPersistence tempPersistence= resolver.resolve(file);
 
-            tempByteArray = Files.readAllBytes(p2pCSVFilePath);
-            patientToPractitionerService.save(new MockMultipartFile(p2pCSVFile.getName(), tempByteArray));
+                bytes = Files.readAllBytes(path);
+                tempPersistence.save(new MockMultipartFile(currentFile.getName(), bytes));
 
-            logger.info("Initial database population successful.");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+                logger.info(progressionOfFilePersistence(file));
+                logger.info(progressFeedback(filesProgress));
+
+                filesProgress++;
+                step++;
+            }
+        } catch (IOException ioe) {
+            logger.error("Error populating database at step: " + step);
+            logger.info(ioe.getMessage());
+        } catch (IllegalPersistStrategy ips) {
+            logger.error(ips.getMessage());
         }
+        logger.info("All persistence actions completed.");
+    }
+
+    private String currentFile(final String file) {
+        return "File: " + file + ".";
+    }
+
+    private String progressionOfFilePersistence(final String file) {
+        return "Persistence of file " + file + " completed.";
+    }
+
+    private String progressFeedback(final int filesSoFar) {
+        return ((double) filesSoFar / filesNames.length) * 100 + "% completed";
     }
 }
