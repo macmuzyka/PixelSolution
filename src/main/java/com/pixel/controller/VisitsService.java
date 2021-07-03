09850cb088bbcc80ed2e17ef06ee955b8c2e588f
macmuzyka <raweshau@gmail.com>
2021-07-03 23:31:35 +0200

    add extracted methods for getting parameterized queries, getting dynamic query

--- src/main/java/com/pixel/controller/VisitsService.java
+++ src/main/java/com/pixel/controller/VisitsService.java
@@ -1,7 +1,7 @@
 package com.pixel.controller;
 
-import com.pixel.model.non_entity_projection.PatientVisitsDTO;
-import com.pixel.model.non_entity_projection.PractitionerVisitsDTO;
+import com.pixel.model.non_entity_projection.PatientVisits;
+import com.pixel.model.non_entity_projection.PractitionerVisits;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
@@ -21,42 +21,63 @@ class VisitsService {
         this.entityManager = entityManager;
     }
 
-    // ALTERNATIVE METHOD TO EXTRACT DTOs FROM DATABASE USING ENTITY MANAGER AND RESULT TRANSFORMER
-    public List<PatientVisitsDTO> findPatientVisits(String city, String specialization) {
-        return this.entityManager.createNativeQuery("SELECT FIRST_NAME, LAST_NAME, COUNT(*) AS VISITS " +
-                "                    FROM PATIENTS p " +
-                "                             INNER JOIN VISITS v ON p.ID = v.PATIENT_ID " +
-                "                             INNER JOIN PRACTITIONERS pr ON v.PRACTITIONER_ID = pr.ID " +
-                "                    WHERE CASE " +
-                "                              WHEN :specialization = 'ALL' OR :specialization = '' THEN " +
-                "                                      pr.SPECIALIZATION LIKE '%' " +
-                "                              ELSE " +
-                "                                      pr.SPECIALIZATION = :specialization " +
-                "                        END " +
-                "                      AND CASE " +
-                "                              WHEN :city = 'ALL' OR :city = '' THEN " +
-                "                                      p.CITY LIKE '%' " +
-                "                              ELSE " +
-                "                                      p.CITY = :city " +
-                "                        END " +
-                "                    GROUP BY FIRST_NAME")
+    public List<PatientVisits> findPatientVisits(List<String> cities, String specialization) {
+        String parameterizedPatientVisitsQuery = getParameterizedQuery(cities, specialization);
+        return entityManager.createNativeQuery(parameterizedPatientVisitsQuery).getResultList();
+    }
+
+    public List<PractitionerVisits> findPractitionerVisits(String specialization) {
+        return this.entityManager.createNativeQuery(getPractitionerVisitsQuery())
                 .setParameter("specialization", specialization)
-                .setParameter("city", city)
                 .getResultList();
     }
 
-    public List<PractitionerVisitsDTO> findPractitionerVisits(String specialziation) {
-        return this.entityManager.createNativeQuery("SELECT pr.SPECIALIZATION, COUNT(*) AS TOTAL_VISITS " +
+    private String getParameterizedQuery(final List<String> cities, final String specialization) {
+        return String.format(
+                "SELECT FIRST_NAME, LAST_NAME, COUNT(*) AS VISITS " +
+                        "FROM PATIENTS p " +
+                        "INNER JOIN VISITS v ON p.ID = v.PATIENT_ID " +
+                        "INNER JOIN PRACTITIONERS pr ON v.PRACTITIONER_ID = pr.ID " +
+                        "WHERE (CASE " +
+                        "WHEN ('%1$s' = 'ALL') OR ('%1$s' = '') " +
+                        "THEN (pr.SPECIALIZATION LIKE '%%') " +
+                        "ELSE (pr.SPECIALIZATION = '%1$s') " +
+                        "END) " +
+                        "AND (CASE " +
+                        "WHEN ('%2$s' = 'ALL') OR ('%2$s' = '') " +
+                        "THEN (p.CITY LIKE '%%') " +
+                        "ELSE (p.CITY IN (%3$s)) " +
+                        "END) " +
+                        "GROUP BY FIRST_NAME",
+                specialization,
+                cities.get(0),
+                getDynamicIN_ClauseQuery(cities));
+    }
+
+    private String getDynamicIN_ClauseQuery(final List<String> cities) {
+        StringBuilder citiesQueryBuilder;
+        if (cities.size() == 0) {
+            citiesQueryBuilder = new StringBuilder("");
+        } else if (cities.size() == 1) {
+            citiesQueryBuilder = new StringBuilder("'" + cities.get(0) + "'");
+        } else {
+            citiesQueryBuilder = new StringBuilder("'" + cities.get(0) + "'");
+            for (int i = 1; i < cities.size(); i++) {
+                citiesQueryBuilder.append(",").append("'").append(cities.get(i)).append("'");
+            }
+        }
+        return citiesQueryBuilder.toString().trim();
+    }
+
+    private String getPractitionerVisitsQuery() {
+        return "SELECT pr.SPECIALIZATION, COUNT(*) AS VISITS " +
                 "FROM PRACTITIONERS pr " +
-                "         INNER JOIN VISITS v ON pr.ID = v.PRACTITIONER_ID " +
+                "INNER JOIN VISITS v ON pr.ID = v.PRACTITIONER_ID " +
                 "WHERE CASE " +
-                "          WHEN :specialization = 'ALL' OR :specialization = '' " +
-                "              THEN pr.SPECIALIZATION LIKE '%' " +
-                "          ELSE " +
-                "              pr.SPECIALIZATION = :specialization " +
-                "          END " +
-                "GROUP BY pr.SPECIALIZATION")
-                .setParameter("specialization", specialziation)
-                .getResultList();
+                "WHEN :specialization = 'ALL' OR :specialization = '' " +
+                "THEN pr.SPECIALIZATION LIKE '%' " +
+                "ELSE pr.SPECIALIZATION = :specialization " +
+                "END " +
+                "GROUP BY pr.SPECIALIZATION";
     }
 }
