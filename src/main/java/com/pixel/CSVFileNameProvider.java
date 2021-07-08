572b280b132bed4e6ea25c27aaac578914b4419b
macmuzyka <raweshau@gmail.com>
2021-07-08 19:00:04 +0200

    add separated class responsible for providing with files names for WarmupDatabasePopulation on application started event

---
+++ src/main/java/com/pixel/CSVFileNameProvider.java
@@ -0,0 +1,27 @@
+package com.pixel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Maciej Muzyka
+ * on 07.07.2021
+ */
+
+class CSVFileNameProvider {
+    private static final String baseResourcesPath = "src/main/resources/";
+    public static final String csvFileExtension = ".csv";
+    private static final String[] filesNames = {"patients", "practitioners", "visits", "patient2practitioner"};
+
+    public List<String> execute() {
+        return populateListWithFilesNames();
+    }
+
+    private List<String> populateListWithFilesNames() {
+        List<String> filesURLs = new ArrayList<>();
+        for (String file : filesNames) {
+            filesURLs.add(baseResourcesPath + file + csvFileExtension);
+        }
+        return filesURLs;
+    }
+}
