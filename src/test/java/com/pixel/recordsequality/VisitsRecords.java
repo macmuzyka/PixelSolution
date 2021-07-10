b5b0ec1c62e8c9e2da47fd1ad87968d96d5c721b
macmuzyka <raweshau@gmail.com>
2021-07-10 18:33:45 +0200

    add unit test class to check for an equality of csv records and list of visit model records, plus minor method extraction and add @DisplayName annotation

--- src/test/java/com/pixel/recordsequality/VisitsRecords.java
+++ src/test/java/com/pixel/recordsequality/VisitsRecords.java
@@ -5,6 +5,7 @@ import com.pixel.model.Visit;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,19 +27,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * on 10.07.2021
  */
 
-class VisitsRecords extends RecordsEqualityTest{
+class VisitsRecords extends RecordsEqualityTest {
 
     @Test
+    @DisplayName("Both CSV Record and Model List for Visit should have an equal number of records")
     @Override
     void numberOfCSVRecordsListShouldBeEqualToModelListSize() throws IOException {
         VisitFile visitFile = new VisitFile();
         File file = new File(filesPaths.get(2));
 
-        byte[] bytes;
-        Path path;
-
-        path = Paths.get(file.getPath());
-        bytes = Files.readAllBytes(path);
+        Path path = Paths.get(file.getPath());
+        byte[] bytes = Files.readAllBytes(path);
 
         MultipartFile multipartFile = new MockMultipartFile(file.getName(), bytes);
 
