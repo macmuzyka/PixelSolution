79288b751d7d08ce2b5af6fcf3fd4477644f5eb3
macmuzyka <raweshau@gmail.com>
2021-07-10 20:31:35 +0200

    add abstract class for base test and initializing resolver object

---
+++ src/test/java/com/pixel/persiststrategy/BaseStrategyResolverTest.java
@@ -0,0 +1,38 @@
+package com.pixel.persiststrategy;
+
+import com.pixel.csvutils.handler.PatientFile;
+import com.pixel.csvutils.handler.PatientToPractitionerFile;
+import com.pixel.csvutils.handler.PractitionerFile;
+import com.pixel.csvutils.handler.VisitFile;
+import com.pixel.csvutils.service.PersistStrategyResolver;
+import com.pixel.model.repository.PatientRepository;
+import com.pixel.model.repository.PatientToPractitionerRepository;
+import com.pixel.model.repository.PractitionerRepository;
+import com.pixel.model.repository.VisitRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by Maciej Muzyka
+ * on 10.07.2021
+ */
+
+abstract class BaseStrategyResolverTest {
+    protected PersistStrategyResolver resolver;
+
+    @BeforeEach
+    void setUp() {
+        resolver = new PersistStrategyResolver(mock(PatientFile.class), mock(PatientRepository.class),
+                mock(VisitFile.class), mock(VisitRepository.class),
+                mock(PractitionerFile.class), mock(PractitionerRepository.class),
+                mock(PatientToPractitionerFile.class), mock(PatientToPractitionerRepository.class));
+    }
+
+    @Test
+    abstract void shouldThrowIllegalPersistStrategyException();
+
+    @Test
+    abstract void shouldReturnOneOfPersistStrategies(String file);
+}
