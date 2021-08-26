d75869fd068bdd2bdeb55af7f2d043978b689a16
macmuzyka <raweshau@gmail.com>
2021-08-26 10:47:35 +0200

    add two method to repository , findAll and findBySpecialization

--- src/main/java/com/pixel/model/repository/PractitionerRepository.java
+++ src/main/java/com/pixel/model/repository/PractitionerRepository.java
@@ -4,6 +4,10 @@ import com.pixel.model.Practitioner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PractitionerRepository extends JpaRepository<Practitioner, Integer> {
+    List<Practitioner> findAll();
+    Practitioner findBySpecialization(String specialization);
 }
