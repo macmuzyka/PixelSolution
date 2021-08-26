15a9ead66fedcfe96d03dba7df70c233e8508c3a
macmuzyka <raweshau@gmail.com>
2021-08-26 10:48:26 +0200

    change @BeforeEach annotation to @BeforeAll annotation in base setUp test method, change method to be static to fulfill @BeforeAll requirements

--- src/test/java/com/pixel/recordsequality/RecordsEqualityTest.java
+++ src/test/java/com/pixel/recordsequality/RecordsEqualityTest.java
@@ -1,18 +1,18 @@
 package com.pixel.recordsequality;
 
 import com.pixel.CSVFileNameProvider;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
 
 abstract class RecordsEqualityTest {
-    protected CSVFileNameProvider provider;
-    protected List<String> filesPaths;
+    protected static CSVFileNameProvider provider;
+    protected static List<String> filesPaths;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         provider = new CSVFileNameProvider();
         filesPaths = provider.execute();
     }
