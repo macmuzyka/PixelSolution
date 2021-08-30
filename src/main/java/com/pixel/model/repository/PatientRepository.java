1fd5e8231c653f4c95089836906973c44d0a6d42
macmuzyka <raweshau@gmail.com>
2021-08-30 08:15:21 +0200

    add two overridden methods to repository(existsById and findById)

--- src/main/java/com/pixel/model/repository/PatientRepository.java
+++ src/main/java/com/pixel/model/repository/PatientRepository.java
@@ -4,7 +4,15 @@ import com.pixel.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
     Patient findPatientByFirstNameAndLastName(String firstName, String lastName);
+
+    @Override
+    boolean existsById(Integer integer);
+
+    @Override
+    Optional<Patient> findById(Integer integer);
 }
--- src/main/java/com/pixel/model/repository/PractitionerRepository.java
+++ src/main/java/com/pixel/model/repository/PractitionerRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PractitionerRepository extends JpaRepository<Practitioner, Integer> {
     List<Practitioner> findAll();
     Practitioner findBySpecialization(String specialization);
+
+    @Override
+    boolean existsById(Integer integer);
+
+    @Override
+    Optional<Practitioner> findById(Integer integer);
 }
